7ecf2d1379824a1d2c57a93cb9a2ccd076d40069
Savvaak <149881959+Savvaak@users.noreply.github.com>
2026-05-29 20:14:49 +1000

    Add files via upload

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import Tablefooter from '@mui/material/TableFooter'
+import { Typography } from '@mui/material';
+
+function Footer () {
+    return(
+        <Typography sx={{textAlign:'center', color:'grey', mt: '20px'}}>
+            Акимов Савва Николаевич, Б9123-09.03.04
+        </Typography>
+    )
+}
+
+export default Footer;
